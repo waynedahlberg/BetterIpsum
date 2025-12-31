71dc97691e15579797370aec3d36d11ee24a96ae
Wayne Dahlberg <waynefj40@gmail.com>
2025-12-31 16:52:51 -0700

    Initial Commit

---
+++ BetterIpsum/BetterIpsumApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterIpsumApp.swift
+//  BetterIpsum
+//
+//  Created by Wayne Dahlberg on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterIpsumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
